c3ca763b6ca0cc3653e4bc0b561533b851aff0dc
Rahul Kumar <1999rahul.py@gmail.com>
2023-02-16 17:58:34 +0530

    init

---
+++ TableStorage.Demo.WebAPI/GroceryItemEntity.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Data.Tables;
+using Azure;
+
+namespace TableStorage.Demo.WebAPI
+{
+    public class GroceryItemEntity : ITableEntity
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }//Partation Key
+        public int Price { get; set; }
+        public string? PartitionKey { get; set; }
+        public string? RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
---
+++ TableStorage.Demo.WebAPI/Services/ITableStorageService.cs
@@ -0,0 +1,9 @@
+﻿namespace TableStorage.Demo.WebAPI.Services
+{
+    public interface ITableStorageService
+    {
+        Task<GroceryItemEntity> GetEntityAsync(string category, string id);
+        Task<GroceryItemEntity> UpsertEntityAsync(GroceryItemEntity entity);
+        Task DeleteEntityAsync(string category, string id);
+    }
+}
